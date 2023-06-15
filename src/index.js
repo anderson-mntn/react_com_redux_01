7ad75fcb5c9990a3d94efc63b3ce3fec846ecc3f
Anderson Amantino <88107035+anderson-mntn@users.noreply.github.com>
2023-06-15 10:59:33 -0300

    add: counterReducer and Provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './components/Header';
-import Counter from './components/Counter';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Header/>
-    <Counter></Counter>
+    <App/>
   </React.StrictMode>
 );
 
